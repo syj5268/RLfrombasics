e52cf698a55c31daeb35f766c178bb76ae26d748
syj5268 <syj5268@kaist.ac.kr>
2024-01-05 15:48:53 +0900

    add comment

--- utils/visualization.py
+++ utils/visualization.py
@@ -17,7 +17,7 @@ def value_visualize(episode, data):
     plt.show()
 
 # policy 시각화 함수 만들기
-def action_visualize(episode, data):
+def action_visualize(data):
     import matplotlib.pyplot as plt
     plt.imshow(data, interpolation='nearest', cmap=plt.get_cmap('Paired'))
     plt.xticks(range(7), [0, 1, 2, 3, 4, 5, 6])
@@ -28,9 +28,11 @@ def action_visualize(episode, data):
         for j in range(7):
             if (j, i) in [(2, 0), (2, 1), (2, 2), (4, 2), (4, 3), (4, 4)]:
                 direction = d_symbols[-1]
+            elif (j, i) == (6, 4):
+                direction = 'G'
             else:
                 direction = int(data[i,j])
                 direction = d_symbols[direction]
             plt.text(j, i, direction, ha="center", va="center", color="black", fontsize=20)
-    plt.title(f"Episode: {episode + 1}")
+    plt.title(f"Optimal Policy")
     plt.show()
